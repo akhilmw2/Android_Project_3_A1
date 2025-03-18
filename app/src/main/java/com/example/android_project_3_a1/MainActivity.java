494c3d4dda1fef8f566a7ca11f926f51a0de16a3
Akhil.Nair <akhilmw2@gmail.com>
2025-03-17 23:41:07 -0500

    implement app 1

--- app/src/main/java/com/example/android_project_3_a1/MainActivity.java
+++ app/src/main/java/com/example/android_project_3_a1/MainActivity.java
@@ -1,6 +1,13 @@
 package com.example.android_project_3_a1;
 
+import static android.content.ContentValues.TAG;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +27,34 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        Button btnAttractions = findViewById(R.id.btn_attractions);
+        Button btnRestaurants = findViewById(R.id.btn_restaurants);
+
+        // Handle Attraction Button Click
+        btnAttractions.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Showing Attractions", Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent("com.example.ATTRACTIONS");
+                sendBroadcast(intent);
+                Log.d(TAG, "Broadcast sent: com.example.ATTRACTIONS");
+            }
+        });
+
+        // Handle Restaurants Button Click
+        btnRestaurants.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Showing Restaurants", Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent("com.example.RESTAURANTS");
+                sendBroadcast(intent);
+                Log.d(TAG, "Broadcast sent: com.example.RESTAURANTS");
+            }
+        });
+
+
     }
+
+
 }
